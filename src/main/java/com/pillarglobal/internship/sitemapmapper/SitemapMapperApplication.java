0814c1363c52286b14ad004c5cb6ebe820b1d783
ana.letca <anadoraletca@gmail.com>
2020-09-02 17:38:04 +0300

    init

---
+++ src/main/java/com/pillarglobal/internship/sitemapmapper/SitemapMapperApplication.java
@@ -0,0 +1,13 @@
+package com.pillarglobal.internship.sitemapmapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitemapMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitemapMapperApplication.class, args);
+	}
+
+}
